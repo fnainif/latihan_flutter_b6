cf36292cb4bb2a45a53355f27ae50c937e3b01c5
fnainif <faridanurainif@gmail.com>
2026-05-25 07:52:01 +0700

    Add: Tugas 8 Flutter

--- lib/latihan/loginkosong.dart
+++ lib/latihan/loginkosong.dart
@@ -7,12 +7,71 @@ class Loginkosong extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 161, 233),
       body: Column(
         children: [
-          Image.asset("assets/images/book2.png"),
+          SizedBox(height: 20),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.asset("assets/images/peony.jpeg", height: 200),
+          ),
+          SizedBox(height: 20),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(100, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Kinday",
+                        style: TextStyle(
+                          fontSize: 50,
+                          color: const Color.fromARGB(255, 83, 43, 63),
+                        ),
+                      ),
+                      Text(
+                        "Deskripsi",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 83, 43, 63),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        "Created by",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 83, 43, 63),
+                        ),
+                      ),
+                      Text(
+                        "Farida Nur Aini",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 83, 43, 63),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Version : 1.0",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 83, 43, 63),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
           HomeButton(
             sosmed: "Back",
-            warnaBox: Color(0xFF062592),
+            warnaBox: const Color.fromARGB(255, 83, 43, 63),
             loginSize: 15,
           ),
           SizedBox(height: 30),
